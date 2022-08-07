a77f0e320c1070020d5b16a791be61d6127ed200
Luisfelipeprusch123 <103533135+Luisfelipeprusch123@users.noreply.github.com>
2022-08-06 21:39:22 -0300

    first commit

---
+++ IDAO.java
@@ -0,0 +1,20 @@
+package com.jdenner.dao;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author luis felipe
+ */
+public interface IDAO<Tipo> {
+
+    public void inserir(Tipo objeto) throws Exception;
+
+    public void alterar(Tipo objeto) throws Exception;
+
+    public void excluir(Tipo objeto) throws Exception;
+
+    public ArrayList<Tipo> listarTodos() throws Exception;
+
+    public Tipo recuperar(int codigo) throws Exception;
+}
